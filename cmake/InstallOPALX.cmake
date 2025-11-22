68688f0e5a718cf162c3654c33baef31f2bf47e8
Mohsen Sadr <mohsen.sadr91@gmail.com>
2025-11-22 23:50:55 +0100

    differentiate opalx exec from lib needed for unit test

--- cmake/InstallOPALX.cmake
+++ cmake/InstallOPALX.cmake
@@ -19,7 +19,16 @@ install(
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/OPALXVersions.h
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/opalx)
 
+# Install the OPALX library
+install(
+  TARGETS opalx
+  EXPORT OPALXTargets
+  LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}      # shared libs
+  ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}      # static libs
+  INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}  # include/opalx
+)
+
 # -------------------------------------------------------
 # Install the actual executable
-install(TARGETS opalx
+install(TARGETS opalx_cli
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
