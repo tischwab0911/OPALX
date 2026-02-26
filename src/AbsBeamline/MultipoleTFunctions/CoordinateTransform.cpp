13362549e77ea3299c9be658b44cb3d7b0e83009
Alexander Liemen <47395086+aliemen@users.noreply.github.com>
2026-02-26 21:24:20 +0100

    Apply clang 20

--- src/AbsBeamline/MultipoleTFunctions/CoordinateTransform.cpp
+++ src/AbsBeamline/MultipoleTFunctions/CoordinateTransform.cpp
@@ -92,10 +92,11 @@ namespace coordinatetransform {
             (lambdaleft_m * std::log(std::cosh((s + s_0_m) / lambdaleft_m))
              - lambdaright_m * std::log(std::cosh((s - s_0_m) / lambdaright_m)))
             / prefactor));
-        result.push_back(std::cos(
-            (lambdaleft_m * std::log(std::cosh((s + s_0_m) / lambdaleft_m))
-             - lambdaright_m * std::log(std::cosh((s - s_0_m) / lambdaright_m)))
-            / prefactor));
+        result.push_back(
+            std::cos(
+                (lambdaleft_m * std::log(std::cosh((s + s_0_m) / lambdaleft_m))
+                 - lambdaright_m * std::log(std::cosh((s - s_0_m) / lambdaright_m)))
+                / prefactor));
         return result;
     }
 
