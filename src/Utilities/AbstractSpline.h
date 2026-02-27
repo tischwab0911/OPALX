edbf16eb8f740411b0bd1ada4bde450e3c443835
Alexander Liemen <47395086+aliemen@users.noreply.github.com>
2026-02-27 21:16:13 +0100

    Also keep the <vector> include

--- src/Utilities/AbstractSpline.h
+++ src/Utilities/AbstractSpline.h
@@ -18,6 +18,7 @@
 #ifndef OPALX_ABSTRACTSPLINE_H
 #define OPALX_ABSTRACTSPLINE_H
 
+#include <vector>
 #include "OPALTypes.h"
 
 /// \brief Base class for the linear and cubic interpolation spline classes
