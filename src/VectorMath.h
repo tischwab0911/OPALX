b5c002221c66ad0d42565f4ab264b14c7ceb67e8
Alexander Liemen <47395086+aliemen@users.noreply.github.com>
2026-03-18 10:56:03 +0100

    Unify dots to use ippl variants. Perhaps later remove them alltogether if we have them from ippl anyways?

--- src/VectorMath.h
+++ src/VectorMath.h
@@ -13,27 +13,19 @@ using VectorPair_t = std::pair<Vector_t<double, 3>, Vector_t<double, 3>>;
 // Euclidean norm for OPALX/IPPL vectors.
 template <class T, unsigned D>
 KOKKOS_INLINE_FUNCTION double euclidean_norm(const Vector_t<T, D>& v) {
-    return Kokkos::sqrt(dot(v, v).apply());
+    return Kokkos::sqrt(v.dot(v));
 }
 
 // Scalar product between two OPALX/IPPL vectors.
 template <class T, unsigned D>
 KOKKOS_INLINE_FUNCTION double dot(const Vector_t<T, D>& v, const Vector_t<T, D>& w) {
-    double res = 0.0;
-    for (unsigned i = 0; i < D; ++i) {
-        res += v(i) * w(i);
-    }
-    return res;
+    return v.dot(w);
 }
 
 // Squared Euclidean norm for OPALX/IPPL vectors.
 template <class T, unsigned D>
 KOKKOS_INLINE_FUNCTION double dot(const Vector_t<T, D>& v) {
-    double res = 0.0;
-    for (unsigned i = 0; i < D; ++i) {
-        res += v(i) * v(i);
-    }
-    return res;
+    return v.dot(v);
 }
 
 #endif
