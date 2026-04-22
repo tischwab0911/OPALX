ddc2c07cd8eb40a95a352be420ff8f932701eea7
Andreas Adelmann <andreas.adelmann@psi.ch>
2026-04-22 21:58:39 +0200

    Rename ClassicRandom to ExpressionRandom
      Rename the legacy expression random generator files to ExpressionRandom
      and update all includes and CMake references. Keep the existing Random
      type and Options::rangen API unchanged to avoid behavioral churn.

---
+++ src/Utilities/ExpressionRandom.cpp
@@ -0,0 +1,143 @@
+// ------------------------------------------------------------------------
+// $RCSfile: ExpressionRandom.cpp,v $
+// ------------------------------------------------------------------------
+// $Revision: 1.1.1.1 $
+// ------------------------------------------------------------------------
+// Copyright: see Copyright.readme
+// ------------------------------------------------------------------------
+//
+// Class: Random:
+//   The OPALX expression random generator.
+// ------------------------------------------------------------------------
+// Class category: Utilities
+// ------------------------------------------------------------------------
+//
+// $Date: 2000/03/27 09:32:38 $
+// $Author: fci $
+//
+// ------------------------------------------------------------------------
+
+#include "Utilities/ExpressionRandom.h"
+#include <cmath>
+
+
+// using a default seed value, initialize the pseudo-random number
+// generator and load the array irn with a set of random numbers in
+// [0, maxran)
+// input: a seed value in the range [0..maxran)
+Random::Random() {
+    init55(123456789);
+}
+
+
+// using the given seed value, initialize the pseudo-random number
+// generator and load the array irn with a set of random numbers in
+// [0, maxran)
+// input: a seed value in the range [0..maxran)
+Random::Random(int seed) {
+    init55(seed);
+}
+
+
+// destructor for random generator.
+Random::~Random()
+{}
+
+
+// set new seed.
+void Random::reseed(int seed) {
+    init55(seed);
+}
+
+
+// return a real pseudo-random number in the range [0,1).
+double Random::uniform() {
+    const double scale = 1.0 / maxran;
+
+    if(next >= nr) irngen();
+
+    return scale * (double) irn[next++];
+}
+
+
+// return two double Gaussioan pseudo-random numbers
+void Random::gauss(double &gr1, double &gr2) {
+    double xi1, xi2, zzr;
+
+    // construct two random numbers in range [-1.0,+1.0)
+    // reject, if zzr > 1
+    do {
+        xi1 = uniform() * 2.0 - 1.0;
+        xi2 = uniform() * 2.0 - 1.0;
+        zzr = xi1 * xi1 + xi2 * xi2;
+    } while(zzr > 1.0);
+
+    // transform accepted point to Gaussian distribution
+    zzr = sqrt(-2.0 * log(zzr) / zzr);
+    gr1 = xi1 * zzr;
+    gr2 = xi2 * zzr;
+}
+
+
+// return an integer pseudo-random number in the range [0,maxran]
+int Random::integer() {
+    if(next > nr) irngen();
+
+    return irn[next++];
+}
+
+
+// initializes the random generator after setting a seed.
+void Random::init55(int seed) {
+    static const int nd = 21;
+    next = 0;
+
+    if(seed < 0)
+        seed = - seed;
+    else if(seed == 0)
+        seed = 1234556789;
+
+    int j = seed % maxran;
+    irn[nr-1] = j;
+    int k = 1;
+
+    for(int i = 1; i < nr; i++) {
+        int ii = (nd * i) % nr;
+        irn[ii-1] = k;
+        k = j - k;
+
+        if(k < 0) k += maxran;
+
+        j = irn[ii-1];
+    }
+
+    // call irngen a few times to "warm it up"
+    irngen();
+    irngen();
+    irngen();
+}
+
+
+// generate the next "nr" elements in the pseudo-random sequence.
+void Random::irngen() {
+    static const int nj = 24;
+    int i, j;
+
+    for(i = 0; i < nj; i++) {
+        j = irn[i] - irn[i+nr-nj];
+
+        if(j < 0) j += maxran;
+
+        irn[i] = j;
+    }
+
+    for(i = nj; i < nr; i++) {
+        j = irn[i] - irn[i-nj];
+
+        if(j < 0) j += maxran;
+
+        irn[i] = j;
+    }
+
+    next = 0;
+}
---
+++ src/Utilities/ExpressionRandom.h
@@ -0,0 +1,81 @@
+#ifndef OPALX_ExpressionRandom_HH
+#define OPALX_ExpressionRandom_HH
+
+// ------------------------------------------------------------------------
+// $RCSfile: ExpressionRandom.h,v $
+// ------------------------------------------------------------------------
+// $Revision: 1.1.1.1 $
+// ------------------------------------------------------------------------
+// Copyright: see Copyright.readme
+// ------------------------------------------------------------------------
+//
+// Class: Random
+//
+// ------------------------------------------------------------------------
+// Class category: Utilities
+// ------------------------------------------------------------------------
+//
+// $Date: 2000/03/27 09:32:38 $
+// $Author: fci $
+//
+// ------------------------------------------------------------------------
+
+// the random numbers are generated in batches of 55
+const int nr = 55;
+
+
+// the random integers are generated in the range [0, MAXRAN)
+static const int maxran = 1000000000;
+
+/// The OPALX expression random generator.
+// This generator is based on:
+// [center]
+// D. Knuth: The Art of Computer Programming, Vol. 2, 2nd edition.
+// [/center]
+
+class Random {
+
+public:
+
+    /// Constructor with default seed.
+    Random();
+
+    /// Constructor with given seed.
+    //  The argument is the new seed, an integer in the range [0, 1000000000].
+    Random(int seed);
+
+    ~Random();
+
+    /// Set a new seed.
+    //  The argument is the new seed, an integer in the range [0, 1000000000].
+    void reseed(int seed = 123456789);
+
+    /// Uniform distribution.
+    //  Return a real pseudo-random number in the range [0,1).
+    double uniform();
+
+    /// Gaussian distribution.
+    //  Return two real Gaussian pseudo-random numbers with unit standard
+    //  deviation.  The values are placed in the two arguments.
+    void gauss(double &gr1, double &gr2);
+
+    /// Uniform distribution.
+    //  Return an integer pseudo-random number in the range [0,1000000000).
+    int integer();
+
+    /// Initialise random number generator.
+    void init55(int seed);
+
+private:
+
+    // generate next batch of random numbers
+    void irngen();
+
+    // a set of "NR" random integers
+    int irn[nr];
+
+    // the next item to be used in the random sequence
+    int next;
+};
+
+#endif // OPALX_ExpressionRandom_HH
