d2fdcb2bcd5d41b0f43f91f232178458fd1252bf
MichZampetakis <michail.zampetakis@psi.ch>
2026-05-12 14:22:25 +0200

    modify src/Fields/Astra1DMagnetoStatic.cpp

--- src/Fields/Astra1DMagnetoStatic.cpp
+++ src/Fields/Astra1DMagnetoStatic.cpp
@@ -1,24 +1,37 @@
 #include "Fields/Astra1DMagnetoStatic.h"
 #include "Fields/Fieldmap.hpp"
+#include "PartBunch/PartBunch.h"
 #include "Physics/Physics.h"
-#include "Utilities/GeneralClassicException.h"
+#include "Utilities/GSLSpline.h"
+#include "Utilities/GeneralOpalException.h"
 #include "Utilities/Util.h"
 
-#include "gsl/gsl_interp.h"
-#include "gsl/gsl_spline.h"
-#include "gsl/gsl_fft_real.h"
-
+#include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <ios>
+#include <sstream>
 
 
-_Astra1DMagnetoStatic::_Astra1DMagnetoStatic(const std::string& filename)
-    : _Fieldmap(filename),
-      FourCoefs_m(nullptr) {
+/**
+ * @brief Constructor for 1D magnetostatic field map.
+ *
+ * Parses the fieldmap header and scans the longitudinal on-axis Bz data.
+ * The constructor determines:
+ * - fieldmap type (`TAstraMagnetoStatic`)
+ * - Fourier interpolation accuracy (`accuracy_m`)
+ * - optional normalization flag (`normalize_m`)
+ * - longitudinal field boundaries (`zbegin_m`, `zend_m`)
+ * - number of valid strictly increasing z samples (`num_gridpz_m`)
+ * - effective mirrored-periodic interpolation length (`length_m`)
+ *
+ * The actual Fourier coefficients are generated later in `readMap()`.
+ */
+Astra1DMagnetoStatic::Astra1DMagnetoStatic(const std::string& filename) : Fieldmap(filename) {
     std::ifstream file;
-    int skippedValues = 0;
     std::string tmpString;
     double tmpDouble;
+    // int skippedValues = 0;
 
     Type = TAstraMagnetoStatic;
 
@@ -28,203 +41,318 @@ _Astra1DMagnetoStatic::_Astra1DMagnetoStatic(const std::string& filename)
         bool parsing_passed = true;
         try {
             parsing_passed = interpretLine<std::string, int>(file, tmpString, accuracy_m);
-        } catch (GeneralClassicException &e) {
-            parsing_passed = interpretLine<std::string, int, std::string>(file, tmpString, accuracy_m, tmpString);
+        } catch (GeneralOpalException& e) {
+            parsing_passed = interpretLine<std::string, int, std::string>(
+                file, tmpString, accuracy_m, tmpString);
 
             tmpString = Util::toUpper(tmpString);
-            if (tmpString != "TRUE" &&
-                tmpString != "FALSE")
-                throw GeneralClassicException("_Astra1DMagnetoStatic::_Astra1DMagnetoStatic",
-                                              "The third string on the first line of 1D field "
-                                              "maps has to be either TRUE or FALSE");
+            if (tmpString != "TRUE" && tmpString != "FALSE")
+                throw GeneralOpalException(
+                        "Astra1DMagnetoStatic::Astra1DMagnetoStatic",
+                        "The third string on the first line of 1D field maps "
+                        "has to be either TRUE or FALSE");
 
             normalize_m = (tmpString == "TRUE");
         }
-        parsing_passed = parsing_passed &&
-                         interpretLine<double, double>(file, zbegin_m, tmpDouble);
 
-        double tmpDouble2 = zbegin_m;
-        while(!file.eof() && parsing_passed) {
-            parsing_passed = interpretLine<double, double>(file, zend_m, tmpDouble, false);
-            if (zend_m - tmpDouble2 > 1e-10) {
-                tmpDouble2 = zend_m;
-            } else if (parsing_passed) {
-                ++ skippedValues;
+        int acceptedValues = 0;
+
+        parsing_passed = parsing_passed && interpretLine<double, double>(file, zbegin_m, tmpDouble);
+
+        double lastAcceptedZ = zbegin_m;
+
+        if (parsing_passed) {
+            ++acceptedValues;
+            zend_m = zbegin_m;
+        }
+
+        while (true) {
+            const bool line_ok = interpretLine<double, double>(file, zend_m, tmpDouble, false);
+            if (!line_ok) {
+                break;
+            }
+
+            if (zend_m - lastAcceptedZ > 1e-10) {
+                lastAcceptedZ = zend_m;
+                ++acceptedValues;
             }
         }
 
-        file.close();
-        num_gridpz_m = lines_read_m - 2 - skippedValues;
+        num_gridpz_m = acceptedValues;
         lines_read_m = 0;
 
         if (!parsing_passed && !file.eof()) {
             disableFieldmapWarning();
             zend_m = zbegin_m - 1e-3;
-            throw GeneralClassicException("_Astra1DMagnetoStatic::_Astra1DMagnetoStatic",
-                                          "An error occured when reading the fieldmap '" + Filename_m + "'");
+            throw GeneralOpalException(
+                    "Astra1DMagnetoStatic::Astra1DMagnetoStatic",
+                    "Error reading fieldmap '" + Filename_m + "'");
+        }
+
+        if (num_gridpz_m < 2) {
+            throw GeneralOpalException(
+                    "Astra1DMagnetoStatic::Astra1DMagnetoStatic",
+                    "Fieldmap must contain at least two valid sampling points");
         }
         length_m = 2.0 * num_gridpz_m * (zend_m - zbegin_m) / (num_gridpz_m - 1);
+
+        file.close();
     } else {
         noFieldmapWarning();
         zbegin_m = 0.0;
-        zend_m = -1e-3;
+        zend_m   = -1e-3;
     }
 }
 
-_Astra1DMagnetoStatic::~_Astra1DMagnetoStatic() {
-    freeMap();
-}
+Astra1DMagnetoStatic::~Astra1DMagnetoStatic() { freeMap(); }
 
-Astra1DMagnetoStatic _Astra1DMagnetoStatic::create(const std::string& filename)
-{
-    return Astra1DMagnetoStatic(new _Astra1DMagnetoStatic(filename));
-}
+void Astra1DMagnetoStatic::readMap() {
+    if (FourCoefs_m.extent(0) != 0) {
+        return;
+    }
 
-void _Astra1DMagnetoStatic::readMap() {
-    if (FourCoefs_m == nullptr) {
-        // declare variables and allocate memory
-    	std::ifstream in;
+    if (num_gridpz_m < 2) {
+        throw GeneralOpalException(
+                "Astra1DMagnetoStatic::readMap",
+                "Fieldmap must contain at least two valid sampling points");
+    }
 
-        bool parsing_passed = true;
+    std::ifstream in(Filename_m.c_str());
+    if (!in.good()) {
+        throw GeneralOpalException(
+                "Astra1DMagnetoStatic::readMap", "Cannot open fieldmap '" + Filename_m + "'");
+    }
 
-        std::string tmpString;
+    const double dz = (zend_m - zbegin_m) / (num_gridpz_m - 1);
 
-        double Bz_max = 0.0;
-        double dz = (zend_m - zbegin_m) / (num_gridpz_m - 1);
-        double tmpDouble = zbegin_m - dz;
+    // CPU arrays - GSL (FFT + spline) is CPU-only
+    double* RealValues = new double[2 * num_gridpz_m];
+    double* zvals      = new double[num_gridpz_m];
 
-        double *RealValues = new double[2 * num_gridpz_m];
-        double *zvals = new double[num_gridpz_m];
+    for (int i = 0; i < 2 * num_gridpz_m; ++i) {
+        RealValues[i] = 0.0;
+    }
 
-        gsl_spline *Bz_interpolant = gsl_spline_alloc(gsl_interp_cspline, num_gridpz_m);
-        gsl_interp_accel *Bz_accel = gsl_interp_accel_alloc();
+    for (int i = 0; i < num_gridpz_m; ++i) {
+        zvals[i] = 0.0;
+    }
 
-        gsl_fft_real_wavetable *real = gsl_fft_real_wavetable_alloc(2 * num_gridpz_m);
-        gsl_fft_real_workspace *work = gsl_fft_real_workspace_alloc(2 * num_gridpz_m);
+    gsl_spline* Bz_interpolant = gsl_spline_alloc(gsl_interp_cspline, num_gridpz_m);
+    gsl_interp_accel* Bz_accel = gsl_interp_accel_alloc();
 
-        FourCoefs_m = new double[2 * accuracy_m - 1];
+    const int size = 2 * accuracy_m - 1;
+    FourCoefs_m    = Kokkos::DualView<double*>("FourCoefs", size);
+    auto coefs     = FourCoefs_m.view_host();
 
-        // read in and parse field map
-        in.open(Filename_m.c_str());
-        getLine(in, tmpString);
+    std::string tmpString;
+    getLine(in, tmpString);
 
-        for (int i = 0; i < num_gridpz_m && parsing_passed;/* skip increment on i here */) {
-            parsing_passed = interpretLine<double, double>(in, zvals[i], RealValues[i]);
-            // the sequence of z-position should be strictly increasing
-            // drop sampling points that don't comply to this
-            if (zvals[i] - tmpDouble > 1e-10) {
-                if (std::abs(RealValues[i]) > Bz_max) {
-                    Bz_max = std::abs(RealValues[i]);
-                }
-                tmpDouble = zvals[i];
-                ++ i; // increment i only if sampling point is accepted
-            }
-        }
-        in.close();
+    double Bz_max        = 0.0;
+    double lastAcceptedZ = zbegin_m - dz;
+    int accepted         = 0;
 
-        gsl_spline_init(Bz_interpolant, zvals, RealValues, num_gridpz_m);
+    while (accepted < num_gridpz_m) {
+        double ztmp  = 0.0;
+        double bztmp = 0.0;
 
-        // get equidistant sampling from the, possibly, non-equidistant sampling
-        // using cubic spline for this
-        int ii = num_gridpz_m;
-        for (int i = 0; i < num_gridpz_m - 1; ++ i, ++ ii) {
-            double z = zbegin_m + dz * i;
-            RealValues[ii] = gsl_spline_eval(Bz_interpolant, z, Bz_accel);
-        }
-        RealValues[ii ++] = RealValues[num_gridpz_m - 1];
-        // prepend mirror sampling points such that field values are periodic for sure
-        -- ii; // ii == 2*num_gridpz_m at the moment
-        for (int i = 0; i < num_gridpz_m; ++ i, -- ii) {
-            RealValues[i] = RealValues[ii];
+        const bool ok = interpretLine<double, double>(in, ztmp, bztmp, false);
+        if (!ok) {
+            break;
         }
 
-        gsl_fft_real_transform(RealValues, 1, 2 * num_gridpz_m, real, work);
-
-        if (!normalize_m) {
-            Bz_max = 1.0;
+        if (ztmp - lastAcceptedZ > 1e-10) {
+            zvals[accepted]      = ztmp;
+            RealValues[accepted] = bztmp;
+            Bz_max               = std::max(Bz_max, std::abs(bztmp));
+            lastAcceptedZ        = ztmp;
+            ++accepted;
         }
-        // normalize to Bz_max = 1 A/m
-        FourCoefs_m[0] = RealValues[0] / (Bz_max * 2. * num_gridpz_m);
-        for (int i = 1; i < 2 * accuracy_m - 1; i++) {
-            FourCoefs_m[i] = RealValues[i] / (Bz_max * num_gridpz_m);
-        }
+    }
+    in.close();
 
+    if (accepted != num_gridpz_m) {
         gsl_spline_free(Bz_interpolant);
         gsl_interp_accel_free(Bz_accel);
-
-        gsl_fft_real_workspace_free(work);
-        gsl_fft_real_wavetable_free(real);
-
         delete[] zvals;
         delete[] RealValues;
 
-        INFOMSG(level3 << typeset_msg("read in fieldmap '" + Filename_m  + "'", "info") << endl);
+        std::ostringstream os;
+        os << "Mismatch between counted and parsed fieldmap points in '" << Filename_m 
+           << "': expected " << num_gridpz_m << ", got " << accepted;
+        throw GeneralOpalException("Astra1DMagnetoStatic::readMap", os.str());
+    }
+
+    if (Bz_max == 0.0) {
+        gsl_spline_free(Bz_interpolant);
+        gsl_interp_accel_free(Bz_accel);
+        delete[] zvals;
+        delete[] RealValues;
+
+        throw GeneralOpalException(
+                "Astra1DMagnetoStatic::readMap",
+                "Maximum on-axis magnetic field is zero in fieldmap '" + Filename_m + "'");
+    }
+
+    gsl_spline_init(Bz_interpolant, zvals, RealValues, num_gridpz_m);
+
+    int ii = num_gridpz_m;
+    for (int i = 0; i < num_gridpz_m - 1; ++i, ++ii) {
+        const double z = zbegin_m + dz * i;
+        RealValues[ii] = gsl_spline_eval(Bz_interpolant, z, Bz_accel);
+    }
+
+     // Ensure periodicity by mirroring
+    RealValues[ii++] = RealValues[num_gridpz_m - 1];
+    --ii;
+    for (int i = 0; i < num_gridpz_m; ++i, --ii) {
+        RealValues[i] = RealValues[ii];
+    }
+
+    // Disable normalization if requested
+    const double norm = normalize_m ? Bz_max : 1.0;
+
+    // Compute Fourier coefficients explicitly from the mirrored periodic samples.
+    const int M = 2 * num_gridpz_m;
+
+    double a0 = 0.0;
+    for (int j = 0; j < M; ++j) {
+        a0 += RealValues[j];
+    }
+    coefs(0) = a0 / (norm * M);
+
+    for (int l = 1; l < accuracy_m; ++l) {
+        double a_l = 0.0;
+        double b_l = 0.0;
+
+        for (int j = 0; j < M; ++j) {
+            const double theta = Physics::two_pi * double(j) / double(M);
+            a_l += RealValues[j] * std::cos(l * theta);
+            b_l += RealValues[j] * std::sin(l * theta);
+        }
+
+        a_l *= 2.0 / double(M);
+        b_l *= 2.0 / double(M);
+
+        const int n = 2 * l - 1;
+
+        coefs(n)     =  a_l / norm;
+        coefs(n + 1) = -b_l / norm;
+    }
+
+    gsl_spline_free(Bz_interpolant);
+    gsl_interp_accel_free(Bz_accel);
+
+    delete[] zvals;
+    delete[] RealValues;
+
+    FourCoefs_m.modify<Kokkos::HostSpace>();
+    FourCoefs_m.sync<Kokkos::DefaultExecutionSpace>();
+
+    Inform m("Astra1DMagnetoStatic::readMap");
+    m << level3 << "Read in fieldmap '" << Filename_m << "'" << endl;
+}
+
+void Astra1DMagnetoStatic::freeMap()
+{
+    if (FourCoefs_m.extent(0) != 0) {
+        FourCoefs_m = Kokkos::DualView<double*>();
+
+        Inform m("Astra1DMagnetoStatic::freeMap");
+        m << level3 << "Freed fieldmap '" << Filename_m << "'" << endl;
     }
 }
 
-void _Astra1DMagnetoStatic::freeMap() {
-    if (FourCoefs_m != nullptr) {
+/**
+ * @brief Apply the FM to all the particles.
+ *
+ * @param pc Particle container
+ */
+void Astra1DMagnetoStatic::applyField(std::shared_ptr<ParticleContainer_t> pc, double scale) {
+    const double zbegin = zbegin_m;
+    const double zend   = zend_m;
+    const double length = length_m;
+    const int accuracy  = accuracy_m;
+    const size_t nLocal = pc->getLocalNum();
 
-        delete[] FourCoefs_m;
-        FourCoefs_m = nullptr;
-    }
+    auto FourCoefs_device = FourCoefs_m.view_device();
+
+    auto Rview = pc->R.getView();
+    auto Eview = pc->E.getView();
+    auto Bview = pc->B.getView();
+
+    Kokkos::parallel_for(
+            "Astra1DMagnetoStatic::applyField", nLocal, KOKKOS_LAMBDA(const size_t i) {
+            const auto& R = Rview(i);
+
+            if (R(2) >= zbegin && R(2) < zend) {
+                Vector_t<double, 3> localE(0.0);
+                Vector_t<double, 3> localB(0.0);
+
+                computeField(
+                        R, localE, localB, FourCoefs_device, zbegin, length,
+                        accuracy);
+
+                Eview(i) += scale * localE;
+                Bview(i) += scale * localB;
+            }
+        });
 }
 
-bool _Astra1DMagnetoStatic::getFieldstrength(const Vector_t &R, Vector_t &/*E*/, Vector_t &B) const {
-    // do fourier interpolation in z-direction
-    const double RR2 = R(0) * R(0) + R(1) * R(1);
+/**
+ * @brief Get the fieldstrength at position R
+ *
+ * @param R Position
+ * @param E Electric Field (unused)
+ * @param B Magnetic Field
+ *
+ * @return true if R is outside of the field map, false otherwise.
+ */
+bool Astra1DMagnetoStatic::getFieldstrength(
+        const Vector_t<double, 3>& R, Vector_t<double, 3>& E, Vector_t<double, 3>& B) const {
+    if (isInside(R)) {
+        computeField(R, E, B, FourCoefs_m.view_host(), zbegin_m, length_m, accuracy_m);
 
-    const double kz = Physics::two_pi * (R(2) - zbegin_m) / length_m + Physics::pi;
-
-    double ez = FourCoefs_m[0];
-    double ezp = 0.0;
-    double ezpp = 0.0;
-    double ezppp = 0.0;
-
-    int n = 1;
-    for (int l = 1; l < accuracy_m ; l++, n += 2) {
-        double somefactor_base =  Physics::two_pi / length_m * l;    // = \frac{d(kz*l)}{dz}
-    	double somefactor = 1.0;
-        double coskzl = cos(kz * l);
-        double sinkzl = sin(kz * l);
-        ez    += (FourCoefs_m[n] * coskzl - FourCoefs_m[n + 1] * sinkzl);
-        somefactor *= somefactor_base;
-        ezp   += somefactor * (-FourCoefs_m[n] * sinkzl - FourCoefs_m[n + 1] * coskzl);
-        somefactor *= somefactor_base;
-        ezpp  += somefactor * (-FourCoefs_m[n] * coskzl + FourCoefs_m[n + 1] * sinkzl);
-        somefactor *= somefactor_base;
-        ezppp += somefactor * (FourCoefs_m[n] * sinkzl + FourCoefs_m[n + 1] * coskzl);
+        return false;
     }
-    // expand the field off-axis
-    const double BfieldR = -ezp / 2. + ezppp / 16. * RR2;
 
-    B(0) += BfieldR * R(0);
-    B(1) += BfieldR * R(1);
-    B(2) += ez - ezpp * RR2 / 4.;
+    return true;
+}
 
+/**
+ * @brief Get the derivative of the field at position R
+ *
+ * @param R Position
+ * @param E Electric Field
+ * @param B Magnetic field
+ *
+ * @note Not implemented yet
+ *
+ */
+bool Astra1DMagnetoStatic::getFieldDerivative(
+        const Vector_t<double, 3>& /*R*/, Vector_t<double, 3>& /*E*/, Vector_t<double, 3>& /*B*/,
+        const DiffDirection& /*dir*/) const
+{
     return false;
 }
 
-bool _Astra1DMagnetoStatic::getFieldDerivative(const Vector_t &/*R*/, Vector_t &/*E*/, Vector_t &/*B*/, const DiffDirection &/*dir*/) const {
-    return false;
-}
-
-void _Astra1DMagnetoStatic::getFieldDimensions(double &zBegin, double &zEnd) const {
+void Astra1DMagnetoStatic::getFieldDimensions(double& zBegin, double& zEnd) const {
     zBegin = zbegin_m;
-    zEnd = zend_m;
-}
-void _Astra1DMagnetoStatic::getFieldDimensions(double &/*xIni*/, double &/*xFinal*/, double &/*yIni*/, double &/*yFinal*/, double &/*zIni*/, double &/*zFinal*/) const {}
-
-void _Astra1DMagnetoStatic::swap()
-{ }
-
-void _Astra1DMagnetoStatic::getInfo(Inform *msg) {
-    (*msg) << Filename_m << " (1D magnetostatic); zini= " << zbegin_m << " m; zfinal= " << zend_m << " m;" << endl;
+    zEnd   = zend_m;
 }
 
-double _Astra1DMagnetoStatic::getFrequency() const {
-    return 0.0;
+void Astra1DMagnetoStatic::getFieldDimensions(
+        double& /*xIni*/, double& /*xFinal*/, double& /*yIni*/, double& /*yFinal*/,
+        double& /*zIni*/, double& /*zFinal*/) const {
+    throw GeneralOpalException("Astra1DMagnetoStatic::getFieldDimensions", "not implemented");
 }
 
-void _Astra1DMagnetoStatic::setFrequency(double /*freq*/)
-{ }
+void Astra1DMagnetoStatic::swap() {}
+
+void Astra1DMagnetoStatic::getInfo(Inform* msg) {
+    (*msg) << Filename_m << " (1D magnetostatic); zini= " << zbegin_m << " m; zfinal= " << zend_m
+           << " m;" << endl;
+}
+
+double Astra1DMagnetoStatic::getFrequency() const { return 0.0; }
+
+void Astra1DMagnetoStatic::setFrequency(double /*freq*/) {}
